6718319caed2ed39961d3bb669392ffbff4682d8
MadMax <shustoffmax@yandex.ru>
2019-09-30 14:53:33 +0300

    hex_score() moved from main

--- net_v2.py
+++ net_v2.py
@@ -1,11 +1,13 @@
 import os
 import shutil
+import math
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow.keras import layers
 from Bio import SeqIO
 
-from parser import Parser
+from fasta_parser import Parser
 
 
 class Nnet:
@@ -115,7 +117,8 @@ class Nnet:
         return labels_out
 
     @classmethod
-    def crossval(cls, fasta_cd, fasta_nc):
+    def crossval_fasta(cls, fasta_cd, fasta_nc):
+        accurs = []
         try:
             os.mkdir('crossval')
         except FileExistsError:
@@ -129,6 +132,7 @@ class Nnet:
         used_cd, used_nc = [], []
         test_len_cd = int(len(records_cd) * 0.1)
         for n in range(1, 11):
+            print('-Splitting coding file ' + str(n) + '/10...')
             locdir = "crossval/fold_" + str(n) + '/'
             os.mkdir(locdir)
             test_records_cd, train_records_cd = [], []
@@ -166,6 +170,7 @@ class Nnet:
         test_len_nc = int(len(records_nc) * 0.1)
         test_nums_nc, train_nums_nc = [], []
         for n in range(1, 11):
+            print('-Splitting noncoding file ' + str(n) + '/10...')
             locdir = "crossval/fold_" + str(n) + '/'
             nums_nc = [m for m in range(len(records_nc))]
             while len(test_nums_nc) < test_len_nc:
@@ -206,25 +211,69 @@ class Nnet:
 
         # training and predicting
         for n in range(1, 11):
+            print('-Processing folds ' + str(n) + '/10...')
             locdir = "crossval/fold_" + str(n) + '/'
 
+            # Parsing test file
+            parser_qr = Parser(locdir + 'test_nc_cd.fasta')
+            parser_qr.parse()
+            qr_feat = parser_qr.gen_feat_tab()
+            qr_rscu = parser_qr.rscu_tab(save=False)
+            hex_in_qr = parser_qr.count_hex()
+
             # Parsing ref coding file
-            print('-Processing ' + cd_file + '...')
-            parser_cd = Parser(cd_file)
+            parser_cd = Parser(locdir + 'test_cd.fasta')
             parser_cd.parse()
             cd_feat = parser_cd.gen_feat_tab()
-            cd_rscu = parser_cd.rscu_tab(save)
+            cd_rscu = parser_cd.rscu_tab(save=False)
             cd_hex_freq = parser_cd.gen_hex_tab()
             hex_in_cd = parser_cd.count_hex()
 
             # Parsing ref noncoding file
-            print('-Processing ' + nc_file + '...')
-            parser_nc = Parser(nc_file)
+            parser_nc = Parser(locdir + 'test_nc.fasta')
             parser_nc.parse()
             nc_feat = parser_nc.gen_feat_tab()
-            nc_rscu = parser_nc.rscu_tab(save)
+            nc_rscu = parser_nc.rscu_tab(save=False)
             nc_hex_freq = parser_nc.gen_hex_tab()
             hex_in_nc = parser_nc.count_hex()
+
+            h_score_nc = Nnet.hex_score(cd_hex_freq, nc_hex_freq, hex_in_nc)
+            h_score_cd = Nnet.hex_score(cd_hex_freq, nc_hex_freq, hex_in_cd)
+            h_score_qr = Nnet.hex_score(cd_hex_freq, nc_hex_freq, hex_in_qr)
+            h_sc_nc_df = pd.DataFrame([nc_feat['Name'], h_score_nc]).T
+            h_sc_cd_df = pd.DataFrame([cd_feat['Name'], h_score_cd]).T
+            h_sc_qr_df = pd.DataFrame([qr_feat['Name'], h_score_qr]).T
+            del nc_hex_freq, cd_hex_freq, hex_in_nc, hex_in_cd, hex_in_qr
+            del h_score_cd, h_score_nc, h_score_qr
+
+            for df in [h_sc_nc_df, h_sc_cd_df, h_sc_qr_df]:
+                df.columns = ['Name', 'Hex_score']
+            cd_feat = pd.merge(cd_feat, h_sc_cd_df, on='Name')
+            nc_feat = pd.merge(nc_feat, h_sc_nc_df, on='Name')
+            qr_feat = pd.merge(qr_feat, h_sc_qr_df, on='Name')
+            query_names = qr_feat.Name
+            del h_sc_cd_df, h_sc_nc_df, h_sc_qr_df
+
+            nnet = Nnet(data_cd=cd_feat.drop(['Name'], axis=1).fillna(0),
+                        data_nc=nc_feat.drop(['Name'], axis=1).fillna(0),
+                        data_qr=qr_feat.drop(['Name'], axis=1).fillna(0),
+                        layers=7,
+                        epochs=10)
+            del cd_feat, nc_feat, qr_feat, qr_rscu, nc_rscu, cd_rscu
+            nnet.preprocessing()
+            nnet.set_model()
+            nnet.train()
+            labels_out = nnet.predict()
+            del nnet
+
+            res = ''
+            for l in range(len(labels_out)):
+                res += query_names[l] + ' ' + str(labels_out[l]) + '\n'
+            with open(locdir + "prediction.txt", 'w') as f_obj:
+                f_obj.write(res)
+            del res
+            accurs.append(Nnet.accuracy(locdir + "prediction.txt", 'Pp', 'CNT'))
+        return accurs
 
     @classmethod
     def accuracy(cls, file, prefix_cd, prefix_nc):
@@ -241,3 +290,43 @@ class Nnet:
             elif prefix_nc in name and pred < 0.5:
                 positive += 1
         return positive / all_lines
+
+    @classmethod
+    def hex_score(cls, ref_cd, ref_nc, query):
+        """counts hexamer score for every
+           sequence in the query list
+           using reference frequencies.
+        """
+        if str(type(ref_cd)) == "<class 'pandas.core.frame.DataFrame'>" and \
+                str(type(ref_nc)) == "<class 'pandas.core.frame.DataFrame'>":
+            scores = []
+            for seq in query:
+                score = 0
+                observ_hex = len(query)
+                for hexamer in seq:
+                    if int(ref_cd[hexamer]) == 0:
+                        score += -1
+                    elif int(ref_nc[hexamer]) == 0:
+                        score += 1
+                    else:
+                        score += math.log10(int(ref_cd[hexamer]) / int(ref_nc[hexamer]))
+                if observ_hex != 0:
+                    score /= observ_hex
+                scores.append(score)
+            return pd.Series(scores)
+        else:
+            scores = []
+            for seq in query:
+                score = 0
+                observ_hex = len(query)
+                for hexamer in seq:
+                    if int(ref_cd[hexamer]) == 0:
+                        score += -1
+                    elif int(ref_nc[hexamer]) == 0:
+                        score += 1
+                    else:
+                        score += math.log10(int(ref_cd[hexamer]) / int(ref_nc[hexamer]))
+                if observ_hex != 0:
+                    score /= observ_hex
+                scores.append(score)
+            return pd.Series(scores)
